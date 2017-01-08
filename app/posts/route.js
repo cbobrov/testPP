815f45ea5f11c70260838826c4d573cc8deeaef4
sergei bobrov <sergei.bobrov@upsilonit.com>
2017-01-09 02:22:54 +0300

    add menu type change - failed

--- app/posts/route.js
+++ app/posts/route.js
@@ -2,10 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
     model() {
-        return Ember.$.getJSON(`https://jsonplaceholder.typicode.com/posts/`);
-                 .then( (allposts) => {
-                  allposts.filter((item, ndex) => index < 21);
-                  return allposts;
-                 }),
-    };
-});
+        return Ember.$.getJSON('https://jsonplaceholder.typicode.com/posts/');
+                //  .then( (allposts) => {
+                //   allposts.filter((item, ndex) => index < 21);
+                //   return allposts;
+                //  })
+    },
+});  

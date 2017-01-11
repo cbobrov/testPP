27f8111475a561abb2ee104b64041119e0e2e3b6
sergei bobrov <sergei.bobrov@upsilonit.com>
2017-01-11 19:59:11 +0300

    select 20 Posts, change Mmenu Type - success

--- app/application/controller.js
+++ app/application/controller.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+    mtype: 'navbar-fixed-top',
+    actions: {
+        setMtype (mtype) {
+            this.set ('mtype', mtype);
+        }
+    }  
 });
--- app/posts/route.js
+++ app/posts/route.js
@@ -2,10 +2,45 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
     model() {
-        return Ember.$.getJSON('https://jsonplaceholder.typicode.com/posts/');
-                //  .then( (allposts) => {
-                //   allposts.filter((item, ndex) => index < 21);
-                //   return allposts;
-                //  })
-    },
+        return Ember.$
+                 .getJSON('https://jsonplaceholder.typicode.com/posts/')
+                 .then( (allposts) => {
+                    // let filteredPosts = allposts.filter((item, index) => index < 20);
+                   return allposts.filter((item, index) => index < 20);
+              //      return (filteredPosts); 
+                    }); 
+    }
 });  
+
+
+/*
+function y(allposts) {
+    return allposts.filter((x) => x > 2);
+}
+
+
+function x(a,b) {
+    return a + b;
+}
+
+const x = function (a, b) {
+    return a + b;
+}
+
+const x = (a, b) => {
+    return a + b;
+}
+
+const x = (a, b) => a + b;
+
+// -----
+const x = (a, b) => {a +b}
+
+const x = (a, b) => {
+    a + b;
+}
+
+const x = function(a, b) {
+    a+b;
+}
+*/
